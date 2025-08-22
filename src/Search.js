68b5d0d26e1843a7674dce250fda4652b9a193eb
stefkaramichos <stephanoskaramichos@gmail.com>
2025-08-22 16:47:28 +0300

    Friday Yay

--- src/Search.js
+++ src/Search.js
@@ -1,40 +1,48 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import useFetch from './useFetch';
+import { useEffect } from 'react';
 
 const Search = () => {
 
     const [search, setSearch] = useState('');
-    const [searchBy, setSearchBy] = useState([]);
+    const [searchBy, setSearchBy] = useState('');
     const [activeBackground, setActiveBackground] = useState(false);
     const [dataReady, setLoading] = useState(false);
+    const [url, setUrl] = useState('https://jsonplaceholder.typicode.com/posts');
+  
+
+    
+    const filterByAttr = (attr) => {
+        setSearchBy(attr);
+        setUrl('https://jsonplaceholder.typicode.com/posts?' + `${attr}_like=${search}`);
+        setActiveBackground(!activeBackground); 
+    }
+ 
+
+    const searchData = (e,attr) => {
+        setSearch(e.target.value);
+        setSearchBy(attr);
+        setUrl('https://jsonplaceholder.typicode.com/posts?' + `${attr}_like=${e.target.value}`);
+    }
+
     const { data: blogs, isPending, error } = useFetch(
-        'https://jsonplaceholder.typicode.com/posts?' + (searchBy.length > 0 ? searchBy.map(attr => `${attr}_like=${search}`).join('&') : `title_like=${search}&body_like=${search}&userId_like=${search}`)
+        url
     );
 
 
-    const searchData = (e) => {
-        setSearch(e.target.value);
-    }
-
-    const filterByAttr = (attr) => {
-        if (!searchBy.includes(attr)){
-            setSearchBy([attr]);
-        }
-        else {
-            const newSearchBy = searchBy.filter(item => item !== attr);
-            setSearchBy(newSearchBy);
-        }
-        setActiveBackground(!activeBackground); 
-    }
-
     return ( 
         <div className="search box-size">
-            <h2>Search</h2>
+            {searchBy ? (
+                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '10px', marginBottom: '20px'}}>
+                    <h2>Searching by: {searchBy}</h2> - 
+                    <span>{ search }</span>
+                </div>
+            ) : <h2>Search</h2>}
              
-            <input type="text" value={ search } onChange={(e) => searchData(e)} placeholder="Search blogs..." />
+            <input type="text" value={ search } onChange={(e) => searchData(e,searchBy)} placeholder="Search blogs..." />
             <div className="search-by">
-                <span onClick={() => filterByAttr('title')} style={searchBy.includes('title') ? {backgroundColor: '#f1356d', color: 'white'} : {}} >
+                <span onClick={() => filterByAttr('title')} style={searchBy == 'title' ? {backgroundColor: '#f1356d', color: 'white'} : {}} >
                     Search by title
                 </span>
                 <span  onClick={() => filterByAttr('body')} style={searchBy.includes('body') ? {backgroundColor: '#f1356d', color: 'white'} : {}} >
@@ -45,7 +53,7 @@ const Search = () => {
                 </span>
             </div>
             <div className="result-blogs ">
-                {search.length > 0 ? !isPending ? blogs.length > 0 ? blogs.map((blog) => (
+                {searchBy ? !isPending ? blogs.length > 0 ? blogs.map((blog) => (
                     <div key={blog.id} className="blog blog-preview">
                         <Link to={`/blogs/${blog.id}`}>
                             <h2>{blog.title}</h2>
@@ -53,7 +61,7 @@ const Search = () => {
                         <p>{blog.body}</p>
                         <p>Written by User {blog.userId}</p>
                     </div>
-                )) : <div>no data found</div> : <div><img width="80" src="/Loading_icon.gif"/></div> : <div>Search for blogs</div>
+                )) : <div>no data found</div> : <div><img width="80" src="/Loading_icon.gif"/></div>  : <div>Search by title, body or userId</div>
                 } 
             </div>
         </div>
